b71e83b5c7361c226661d3fa48a0a991d887240b
Shayne Hartford <shaybox@shaybox.com>
2021-03-14 09:21:54 -0400

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,7 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from keyboard import char_to_keycode
+from keyboard import keycode_dict
 
 debug = True
 
@@ -29,108 +29,83 @@ def process_line(line):
         if debug: print("INST:", inst)
 
         if inst == "REM":
-            return;
-
+            return
+        
         global default_delay
+        global previous_line
+
+        if default_delay > 0:
+            time.sleep(default_delay / 1000)
+
         if inst == "DEFAULT_DELAY" or inst == "DEFAULTDELAY":
             try:
                 default_delay = int(args[1])
             except ValueError:
                 print("Invalid integer")
                 return
-
-        if default_delay > 0:
-            time.sleep(default_delay / 1000)
-
-        if inst == "DELAY":
+        elif inst == "DELAY":
             try:
                 time.sleep(int(args[1]) / 1000)
             except ValueError:
                 print("Invalid integer")
                 return
-
-        if inst == "STRING":
+        elif inst == "STRING":
             layout.write(args[1])
-
-        if inst == "GUI" or inst == "WINDOWS":
-            kbd.send(Keycode.WINDOWS, char_to_keycode(args[1]))
-
-        if inst == "APP" or inst == "MENU":
+        elif inst == "GUI" or inst == "WINDOWS":
+            kbd.send(Keycode.WINDOWS, keycode_dict.get(args[1]))
+        elif inst == "APP" or inst == "MENU":
             kbd.send(Keycode.APPLICATION)
-
-        if inst == "SHIFT":
-            kbd.send(Keycode.SHIFT, char_to_keycode(args[1]))
-
-        if inst == "ALT":
-            kbd.send(Keycode.ALT, char_to_keycode(args[1]))
-
-        if inst == "CONTROL" or inst == "CTRL":
-            kbd.send(Keycode.CONTROL, char_to_keycode(args[1]))
-
-        if inst == "DOWNARROW" or inst == "DOWN":
+        elif inst == "SHIFT":
+            kbd.send(Keycode.SHIFT, keycode_dict.get(args[1]))
+        elif inst == "ALT":
+            kbd.send(Keycode.ALT, keycode_dict.get(args[1]))
+        elif inst == "CONTROL" or inst == "CTRL":
+            kbd.send(Keycode.CONTROL, keycode_dict.get(args[1]))
+        elif inst == "DOWNARROW" or inst == "DOWN":
             kbd.send(Keycode.DOWN_ARROW)
-
-        if inst == "LEFTARROW" or inst == "LEFT":
+        elif inst == "LEFTARROW" or inst == "LEFT":
             kbd.send(Keycode.LEFT_ARROW)
-
-        if inst == "RIGHTARROW" or inst == "RIGHT":
+        elif inst == "RIGHTARROW" or inst == "RIGHT":
             kbd.send(Keycode.RIGHT_ARROW)
-
-        if inst == "UPARROW" or inst == "UP":
+        elif inst == "UPARROW" or inst == "UP":
             kbd.send(Keycode.UP_ARROW)
-
-        if inst == "BREAK" or inst == "PAUSE":
+        elif inst == "BREAK" or inst == "PAUSE":
             kbd.send(Keycode.PAUSE)
-
-        if inst == "CAPSLOCK":
+        elif inst == "CAPSLOCK":
             kbd.send(Keycode.CAPS_LOCK)
-
-        if inst == "DELETE":
+        elif inst == "DELETE":
             kbd.send(Keycode.DELETE)
-
-        if inst == "ESC" or inst == "ESCAPE":
+        elif inst == "ESC" or inst == "ESCAPE":
             kbd.send(Keycode.ESCAPE)
-
-        if inst == "HOME":
+        elif inst == "HOME":
             kbd.send(Keycode.HOME)
-
-        if inst == "INSERT":
+        elif inst == "INSERT":
             kbd.send(Keycode.INSERT)
-
-        if inst == "NUMLOCK":
+        elif inst == "NUMLOCK":
             kbd.send(Keycode.KEYPAD_NUMLOCK)
-
-        if inst == "PAGEUP":
+        elif inst == "PAGEUP":
             kbd.send(Keycode.PAGE_UP)
-
-        if inst == "PAGEDOWN":
+        elif inst == "PAGEDOWN":
             kbd.send(Keycode.PAGE_DOWN)
-
-        if inst == "PRINTSCREEN":
+        elif inst == "PRINTSCREEN":
             kbd.send(Keycode.PRINT_SCREEN)
-
-        if inst == "SCROLLLOCK":
+        elif inst == "SCROLLLOCK":
             kbd.send(Keycode.SCROLL_LOCK)
-
-        if inst == "SPACE":
+        elif inst == "SPACE":
             kbd.send(Keycode.SPACEBAR)
-
-        if inst == "TAB":
+        elif inst == "TAB":
             kbd.send(Keycode.TAB)
-
-        if inst == "FN":
+        elif inst == "FN":
             print("FN Instruction can't be implimented, CircuitPython doesn't provide the macOS FN keycode, this is pretty useless anyway")
-
-        global previous_line
-        if inst == "REPEAT":
+        elif inst == "REPEAT":
             try:
                 for _ in range(int(args[1])):
                     process_line(previous_line)
             except ValueError:
                 print("Invalid integer")
                 return
-        else:
-            previous_line = line
+
+        previous_line = line
 
 for line in lines:
     process_line(line)
